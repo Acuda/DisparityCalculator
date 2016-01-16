4dc061b874e008c332564d69e579e83a8c237063
Acuda <eistel@gmail.com>
2016-01-16 18:23:27 +0100

    final

--- MainGUI.py
+++ MainGUI.py
@@ -18,7 +18,11 @@ from configuration import *
 class ValueBar(QtGui.QWidget):
     def __init__(self):
         super(ValueBar, self).__init__()
+        self._timer = QtCore.QTimer(self)
+        self._timer.setSingleShot(True)
+        self._timer.timeout.connect(self.updateValueInstant)
         self.initUI()
+
 
     def initUI(self):
 
@@ -51,6 +55,7 @@ class ValueBar(QtGui.QWidget):
         self.connect(self.slider_nk, QtCore.SIGNAL('valueChanged(int)'), self.sliderChanged)
         self.connect(self.minValue, QtCore.SIGNAL('textChanged(QString)'), self.minValueChanged)
         self.connect(self.maxValue, QtCore.SIGNAL('textChanged(QString)'), self.maxValueChanged)
+        self.connect(self.value, QtCore.SIGNAL("textChanged(QString)"), self.updateValue)
 
         self.initValues()
         self.show()
@@ -79,12 +84,25 @@ class ValueBar(QtGui.QWidget):
         self.box.setTitle(title)
 
     def updateValue(self, value):
+        self.last_value = value
+        self._timer.stop()
+        self._timer.start(500)
+
+    def updateValueInstant(self, value=None):
+
+        if value is None:
+            value = self.last_value
+
+        value = float(value)
         ival = int(value)
         nkval = int(value*1e3 - ival*1e3)
 
         self.value.setText('%d.%03d' % (ival, nkval))
         self.slider.setValue(ival)
         self.slider_nk.setValue(nkval)
+
+        self.slider.setValue(ival)
+        self.slider_nk.setValue(int(value*1e3 - ival*1e3))
 
 
 class EquationBox(QtGui.QWidget):
@@ -93,6 +111,10 @@ class EquationBox(QtGui.QWidget):
         self.name = name
         self.equation_data = equation_data
         self.initUI()
+
+        self._timer = QtCore.QTimer(self)
+        self._timer.setSingleShot(True)
+        self._timer.timeout.connect(self.calculateEquationInstant)
 
     def initUI(self):
         self.grid_layout = QtGui.QGridLayout()
@@ -119,10 +141,10 @@ class EquationBox(QtGui.QWidget):
             bar = ValueBar()
             bar.initValues(title='%s [%s]' % (symbol_name, equation_symbol.unit),
                            value=equation_symbol.value, min=equation_symbol.min, max=equation_symbol.max)
+            QtCore.QObject.connect(bar.value, QtCore.SIGNAL("textChanged(QString)"), self.calculateEquation)
             self.grid_layout.addWidget(bar, idx*scale, 0, scale, 1)
             self.equation_data['ValueBar'].append(bar)
 
-            QtCore.QObject.connect(bar.value, QtCore.SIGNAL("textChanged(QString)"), self.calculateEquation)
 
             radio = QtGui.QRadioButton()
             radio.setChecked(equation_symbol.isResult)
@@ -140,10 +162,13 @@ class EquationBox(QtGui.QWidget):
         QtCore.QObject.connect(button, QtCore.SIGNAL ('clicked()'), self.calculateEquation)
         self.grid_layout.addWidget(button, (idx+1)*scale, 0)
 
-
         self.show()
 
     def calculateEquation(self):
+        self._timer.stop()
+        self._timer.start(50)
+
+    def calculateEquationInstant(self):
         target_idx = -1
         for idx, symbol_name in enumerate(self.equation_data['name_order']):
             new_value = float(self.equation_data['ValueBar'][idx].value.text())
@@ -158,9 +183,10 @@ class EquationBox(QtGui.QWidget):
 
         target_factor = 1.0/self.equation_data['DATA'].symbol_dict[target_symbol].factor
         target_unit = self.equation_data['DATA'].symbol_dict[target_symbol].unit
-        self.equation_data['ValueBar'][target_idx].updateValue(result*target_factor)
+        self.equation_data['ValueBar'][target_idx].updateValueInstant(result*target_factor)
 
         self.equation_data['lblResult'].setText('Result: % 7.3f %s' % (result*target_factor, target_unit))
+
 
 
 
@@ -170,31 +196,43 @@ class MainWindow(QtGui.QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Tooltips')
+        self.setWindowTitle('Disparity Calculator')
 
         self.hlayout = QtGui.QHBoxLayout(self)
         self.vlayout = QtGui.QVBoxLayout()
         self.hlayout.addLayout(self.vlayout)
 
+        EQUATION_DATA['EquationBox'] = list()
+        for name in EQUATION_ORDER:
+            data = EQUATION_DATA[name]
+
         # create EquationBox's
-        for name, data in EQUATION_DATA.items():
+        #for name, data in EQUATION_DATA.items():
             eqb = EquationBox(name, data)
             self.hlayout.addWidget(eqb)
+            EQUATION_DATA['EquationBox'].append(eqb)
             eqb.calculateEquation()
 
+
         # link external values on change
-        for name, data in EQUATION_DATA.items():
+        #for name, data in EQUATION_DATA.items():
             for symbol_name, equation_symbol in data['DATA'].symbol_dict.items():
                 if equation_symbol.extern:
                     idx = EQUATION_DATA[equation_symbol.extern]['name_order'].index(str(symbol_name))
                     bar = EQUATION_DATA[equation_symbol.extern]['ValueBar'][idx]
 
                     idx = EQUATION_DATA[name]['name_order'].index(str(symbol_name))
-                    EQUATION_DATA[name]['ValueBar'][idx].updateValue(2)
+                    #EQUATION_DATA[name]['ValueBar'][idx].updateValue(2)
+
+                    EQUATION_DATA[name]['ValueBar'][idx].value.setText(bar.value.text())
 
                     QtCore.QObject.connect(bar.value, QtCore.SIGNAL("textChanged(QString)"), EQUATION_DATA[name]['ValueBar'][idx].value.setText)
 
+        #EQUATION_DATA['EquationBox'][0].calculateEquation()
+
         self.show()
+
+
         return
 
 
--- configuration.py
+++ configuration.py
@@ -10,30 +10,37 @@
 
 from Equation import *
 
+EQUATION_ORDER = [
+    'Pixel-Size',
+    'DISPARITY',
+    'ERROR DISPARITY',
+]
+
 EQUATION_DATA = {
+    'Pixel-Size': {
+        'DATA': Equation('PixelSize = SensorLength / NumPixel ', [
+            EquationSymbol(name='PixelSize', min=1, max=20, value=17, unit=u'µm', factor=1e-6, isResult=True),
+            EquationSymbol(name='SensorLength', min=1, max=50, value=17.408, unit='mm', factor=1e-3),
+            EquationSymbol(name='NumPixel', min=600, max=2048, value=1024, unit='px'),
+        ])
+    },
     'DISPARITY': {
-        'DATA': Equation('Disparity = (Focal * 1/PixelSize) * Base / Distance ', [
-            EquationSymbol(name='Disparity', min=0, max=200, value=0, unit='px', isResult=True),
-            EquationSymbol(name='Base', min=1, max=200, value=66.6, unit='cm', factor=1e-2),
+        'DATA': Equation('Disparity = (Focal * 1/PixelSize) * Base / Distance', [
+            EquationSymbol(name='Disparity', min=0, max=160, value=64, unit='px', isResult=True),
+            EquationSymbol(name='Base', min=1, max=200, value=43.520, unit='cm', factor=1e-2),
             EquationSymbol(name='Focal', min=1, max=30, value=15, unit='mm', factor=1e-3),
-            EquationSymbol(name='Distance', min=0, max=200, value=5, unit='m'),
+            EquationSymbol(name='Distance', min=6, max=100, value=6, unit='m'),
             EquationSymbol(name='PixelSize', min=1, max=20, value=17, unit=u'µm', factor=1e-6, extern='Pixel-Size'),
         ])
     },
-    'ERROR DISP': {
-        'DATA': Equation('Error = Accuracy ', [
-            EquationSymbol(name='Accuracy', min=0, max=10, value=1./16, unit='px', isResult=True),
-            EquationSymbol(name='Base', min=1, max=200, value=33, unit='cm', factor=1e-2),
-            EquationSymbol(name='Focal', min=1, max=100, value=4, unit='mm', factor=1e-3),
-            EquationSymbol(name='Distance', min=0, max=200, value=5, unit='m'),
-            EquationSymbol(name='PixelSize', min=1, max=20, value=17.7, unit=u'µm', factor=1e-6),
-        ])
-    },
-    'Pixel-Size': {
-        'DATA': Equation('PixelSize = SensorLength / NumPixel ', [
-            EquationSymbol(name='PixelSize', min=1, max=20, value=17, unit=u'µm', factor=1e-6),
-            EquationSymbol(name='SensorLength', min=1, max=50, value=33, unit='mm', factor=1e-3, isResult=True),
-            EquationSymbol(name='NumPixel', min=600, max=2048, value=1024, unit='px'),
+    'ERROR DISPARITY': {
+        'DATA': Equation('DistanceError = (Focal * 1/PixelSize) * Base / Disparity - (Focal * 1/PixelSize) * Base / (Disparity + AccuracyFactor*1./16)', [
+            EquationSymbol(name='DistanceError', min=0, max=100, value=5, unit='m', factor=1, isResult=True),
+            EquationSymbol(name='AccuracyFactor', min=1, max=10, value=5),
+            EquationSymbol(name='Disparity', min=0, max=160, value=0, unit='px', extern='DISPARITY'),
+            EquationSymbol(name='Base', min=1, max=200, value=33, unit='cm', factor=1e-2, extern='DISPARITY'),
+            EquationSymbol(name='Focal', min=1, max=100, value=4, unit='mm', factor=1e-3, extern='DISPARITY'),
+            EquationSymbol(name='PixelSize', min=1, max=20, value=17.7, unit=u'µm', factor=1e-6, extern='DISPARITY'),
         ])
     },
 }
